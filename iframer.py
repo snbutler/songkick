e2bf57e74bfd077c00ca7d729e2e77a4a8a89848
snbutler <simon.noel.butler@gmail.com>
2018-12-04 16:27:43 +0000

    use cache; formatted sk frame

--- iframer.py
+++ iframer.py
@@ -4,41 +4,51 @@ from   sys      import stderr
 from   code     import interact
 from   argparse import ArgumentParser
 from   flask    import Flask, render_template, url_for, session, g
+from   werkzeug.contrib.cache import SimpleCache
 from   songkick import *
 from   setlist  import *
-import re
 import json
 import pprint
+
+app = Flask(__name__)
+app.secret_key = b'_6#yxL"FeQ@z\n\xec]/'
+cache = SimpleCache()
 
 def rev_date(date):
     (d, m, y) = date.split("-")
     return "{}-{}-{}".format(y, m, d)
-
-app = Flask(__name__)
-app.secret_key = b'_6#yxL"FeQ@z\n\xec]/'
-
 def check_session():
     print("check_session")
     print(session.keys())
+
     if not "sk" in session:
         print("initialise sk key")
         with open("sk_api") as f:
-            session.sk = f.readline().strip()
+            session["sk"] = f.readline().strip()
 
     if not "sl" in session:
         print("initialise sl key")
         with open("sl_api") as f:
-            session.sl = f.readline().strip()
+            session["sl"] = f.readline().strip()
 
-    if not "gigs" in session:
-        # gigs = sk.getUserGigs(args.user)
-        print("load from cache")
-        with open("sk.cache") as cache:
-            session["gigs"] = json.load(cache)["songkick"]
+    g.sk = Songkick(session["sk"])
+    g.sl = Setlist(session["sl"])
 
-    g.sk = Songkick(session.sk)
-    g.sl = Setlist(session.sl)
     #interact(local=locals())
+def get_gigs():
+    print("has: ", cache.has("gigs"))
+    gigs = cache.get("gigs")
+    if not gigs:
+        print("load from disk cache")
+        # sk = g.sk.getUserGigs(args.user)
+        with open("sk.cache") as sk_cache:
+            gigs = json.load(sk_cache)["songkick"]
+            cache.set("gigs", gigs, timeout=0)
+            print("has: ", cache.has("gigs"))
+    else:
+        print("found mem cache")
+
+    return gigs
 
 @app.route("/user")
 @app.route("/user/<username>")
@@ -47,50 +57,57 @@ def hello(username=None):
 
 @app.route("/gig")
 @app.route("/gig/<int:n>")
-@app.route("/gig/<int:n>/<int:m>")
-def gig(n=0, m=0):
+def gig(n=0):
     check_session()
     print("checked")
 
-    if n < 0 or n > len(session["gigs"]):
-        return render_template('error.html', n=len(session["gigs"]))
+    gigs = get_gigs()
+
+    if not gigs:
+        return render_template('error.html')
+    elif n < 0 or n > len(gigs):
+        return render_template('error.html', n=len(gigs))
     else:
         print("valid n")
-        gig = session["gigs"][n]
         if not "gig_n" in session or session["gig_n"] != n:
-            print("populating session")
+            print("finding setlists")
+            #pp = pprint.PrettyPrinter()
             session["gig_n"]    = n
-            session["setlists"] = []
-            #interact(local=locals())
-            for p in gig["performance"]:
-                for s in g.sl.findGig(artistName=p["artist"]["displayName"], date=rev_date(gig["start"]["date"])):
-                    session["setlists"].append(s["url"])
+            session["setlists"] = {}
+            for p in gigs[n]["performance"]:
+                sk_name = p["artist"]["displayName"]
+                if not sk_name in session["setlists"]:
+                    session["setlists"][sk_name] = []
+                for s in g.sl.findGig(artistName=sk_name, date=rev_date(gigs[n]["start"]["date"])):
+                    #interact(local=locals())
+                    session["setlists"][sk_name].append({"sl_name": s["artist"]["name"],
+                                                         "sl_url":  s["url"]})
+                if not session["setlists"][sk_name]:
+                    session["setlists"][sk_name].append({"sl_name": "create",
+                                                         "sl_url":  "https://www.setlist.fm/edit"})
 
-        if not "setlists" in session or m < 0 or m > len(session["setlists"]):
-            print("invalid m")
-            return render_template('error.html', n=len(session["setlists"]))
-        else:
-            print("rendering")
-            #interact(local=locals())
+        print("rendering")
+        sk_data = {"details":  [gigs[n]["displayName"],
+                                gigs[n]["start"]["date"],
+                                gigs[n]["end"]["date"] if "end" in gigs[n] else None,
+                                gigs[n]["venue"]["displayName"],
+                                gigs[n]["location"]["city"],
+                                gigs[n]["type"]
+                               ],
+                   "setlists": session["setlists"]
+                  }
 
-            pp = pprint.PrettyPrinter()
+        # interact(local=locals())
+        arb = next(iter((session["setlists"]).keys()))
+        sl_url = session["setlists"][arb][0]["sl_url"] if session["setlists"] else "https://www.setlist.fm/edit"
 
-            prev_n = n-1 if n > 0 else None
-            next_n = n+1 if n < len(session["gigs"])-1 else None
-            prev_m = m-1 if m > 0 else None
-            next_m = m+1 if m < len(session["setlists"])-1 else None
+        return render_template("iframer.html", 
+                               sk_data = sk_data,
+                               sl_url  = sl_url,
+                               n       = n,
+                               max_n   = len(gigs),
+                              )
 
-            print(session["setlists"][m])
-
-            return render_template('iframer.html', 
-                                   #sk_url = re.sub("http:", "https:", re.sub("\?.*", "", gig["uri"])), 
-                                   sk_data = pp.pformat(gig),
-                                   sl_url  = session["setlists"][m],
-                                   n       = n,
-                                   m       = m,
-                                   max_n   = len(session["gigs"]),
-                                   max_m   = len(session["setlists"])
-                                  )
 
 with app.test_request_context():
     pass
